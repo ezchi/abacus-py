7d4dab403a4522f977d50c4ebfa37d1b32d3c6ea
Enzo Chi <enzo.chi.git@gmail.com>
2013-09-21 21:57:15 +1000

    initial commit

---
+++ abacus.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+import random
+import datetime
+import argparse
+
+def cmd_parser():
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--questions",
+                        type=int,
+                        dest="questions",
+                        default=10,
+                        help="Number of questions")
+    parser.add_argument("--operands",
+                        type=int,
+                        dest="operands",
+                        default=4,
+                        help="Number of operands")
+    parser.add_argument("--repeat",
+                        action="store_true",
+                        default=False,
+                        dest="repeat",
+                        help="Repeat the question with wrong answer until get it right")
+    args = parser.parse_args()
+    return args
+
+    
+def gen_questions(num_operands, max_operand=100):
+    """
+    Generate abacus question with given number of operands
+    """
+    operand = 0
+    sum     = 0
+    tmp_sum = 0
+    operands = []
+    
+    for i in range(num_operands):
+        get_operand = False
+        while (not get_operand):
+            operand = random.randint(-sum, max_operand-1)
+            tmp_sum = sum + operand
+            if tmp_sum in range(max_operand):
+                get_operand = True
+                operands.append(operand)
+                sum = tmp_sum
+                
+    return operands, sum
+
+def print_operands(operands):
+    """
+    Print the operands in formula format
+    """
+    for i in range(len(operands)):
+        if (i == 0):
+            print(operands[i], end="")
+        else:
+            if (operands[i] >= 0):
+                print(" + ", end="")
+                print(operands[i], end="")
+            else:
+                print(" - ", end="")
+                print(-operands[i], end="")
+    print(" = ")
+
+
+def repr_operands(operands):
+    """
+    Covert the operands to formula format
+    """
+    rslt = ""
+    for i in range(len(operands)):
+        if (i == 0):
+            rslt = "{0:>2}".format(operands[i])
+        else:
+            if (operands[i] >= 0):
+                rslt += " + {0:>2}".format(operands[i])
+            else:
+                rslt += " - {0:>2}".format(-operands[i])
+    rslt += " = "
+    return rslt
+    
+if __name__ == '__main__':
+    args = cmd_parser()
+
+    num_questions = args.questions
+    not_repeat    = not args.repeat
+    num_operands  = args.operands
+    
+    total_mark = 0
+    wrong_ans  = []
+    
+    log_fname = "wrong_questions.txt"
+    log_fhd   = open(log_fname, 'w')
+
+    start_time = datetime.datetime.now()
+    
+    for i in range(num_questions):
+        mark = 1
+        operands, sum = gen_questions(num_operands)
+        correct = False
+        while not correct:
+            ans = input(repr_operands(operands))
+            if (ans.isdecimal()):
+                if (int(ans) == sum):
+                    correct = True
+                else:
+                    log_fhd.write(repr_operands(operands))
+                    log_fhd.write(ans.strip())
+                    log_fhd.write("\t" + "[{0:>2d}]".format(sum))
+                    log_fhd.write("\n")
+                    mark = 0
+                    if (not_repeat):
+                        correct = True
+                    else:
+                        print("The answer is wrong, try again")
+            else:
+                print("Sorry, give me the answer please")
+                mark = 0
+            total_mark += mark
+
+    end_time = datetime.datetime.now()
+
+    delta_time = end_time - start_time
+    log_fhd.close()
+
+    print("Time       : %s" % delta_time)
+    print("Total Mark : {0:>2.2f}%".format(total_mark/num_questions * 100))
+
+    input()
+        
+        
+    
